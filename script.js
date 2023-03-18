0831faf5f2daa4a9fedd4705a43189f8a96ba065
RohitKewat <rohit554514@gmail.com>
2023-03-18 10:55:06 +0530

    added restart functinality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-let turn = "X"
+let turn = "O"
 let conditionX = [];
 let conditionO = [];
 let result = null
@@ -11,7 +11,7 @@ const checkWinner = (boxNumber, turn) => {
         result = winCondition(conditionX);
         if (result) {
             document.getElementById("result").innerHTML = "X is winner"
-            checkDouble = [1,2,3,4,5,6,7,8,9]
+            checkDouble = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
         }
     } else {
@@ -19,41 +19,53 @@ const checkWinner = (boxNumber, turn) => {
         result = winCondition(conditionO);
         if (result) {
             document.getElementById("result").innerHTML = "O is winner"
-            checkDouble = [1,2,3,4,5,6,7,8,9]
+            checkDouble = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
         }
     }
     console.log(conditionO);
     if (conditionO.length == 5 || conditionX.length == 5) {
         document.getElementById("result").innerHTML = "Game Draw"
-        checkDouble = [1,2,3,4,5,6,7,8,9]
+        checkDouble = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     }
 
 }
 let checkDouble = []
 const handleClick = (boxNumber) => {
-    if(checkDouble.includes(boxNumber)){
-        return 
+    if (checkDouble.includes(boxNumber)) {
+        return
     }
     checkDouble.push(boxNumber)
     if (turn === "X") {
-            let cell = document.getElementById(`cell${boxNumber}`).innerHTML = "O"
-            checkWinner(boxNumber, "O")
-            document.getElementById("turn-info").innerHTML = "Playes X to Move"
-            turn = "O"
+        let cell = document.getElementById(`cell${boxNumber}`).innerHTML = "O"
+        checkWinner(boxNumber, "O")
+        document.getElementById("turn-info").innerHTML = "Playes X to Move"
+        turn = "O"
 
-        } else {
-            let cell = document.getElementById(`cell${boxNumber}`).innerHTML = "X"
-            checkWinner(boxNumber, "X")
-            document.getElementById("turn-info").innerHTML = "Playes O to Move"
+    } else {
+        let cell = document.getElementById(`cell${boxNumber}`).innerHTML = "X"
+        checkWinner(boxNumber, "X")
+        document.getElementById("turn-info").innerHTML = "Playes O to Move"
 
-            turn = "X"
-        }
+        turn = "X"
+    }
 
 }
 
 const handelRestart = () => {
-    location.reload();
+    // location.reload();
+    let cells = document.getElementsByClassName('cell')
+    for (let cell of cells) {
+        cell.innerHTML = ""
+    }
+    document.getElementById("turn-info").innerHTML = "Playes X to Move"
+    document.getElementById("result").innerHTML = ""
+
+    checkDouble = []
+     turn = "O"
+     conditionX = [];
+     conditionO = [];
+     result = null
 }
 
 function winCondition(array) {
